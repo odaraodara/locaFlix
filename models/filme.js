fbcfd04e4be2165045b74747528caf3e48facf05
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-09-14 23:44:29 +0000

    refatorando a criação dos filmes iniciais.

--- models/filme.js
+++ models/filme.js
@@ -1,36 +1,24 @@
 class Filme {
-    //classe dos objetos manipulados
-    constructor(id, nome, duracao, genero, sinopse) {
-      this.id = id;
-      this.nome = nome;
-      this.duracao = duracao;
-      this.genero = genero;
-      this.sinopse = sinopse
-    }
+  //classe dos objetos manipulados
+  constructor(id, nome, duracao, genero, sinopse) {
+    this.id = id;
+    this.nome = nome;
+    this.duracao = duracao;
+    this.genero = genero;
+    this.sinopse = sinopse
   }
+}
 
-  //Objetos iniciais
-const filme1 = {
-    id: 1,
-    nome: "Titanic",
-    duracao: 194,
-    genero: "drama",
-    sinopse:
-      `Um artista pobre e uma jovem rica se conhecem e se apaixonam na fatídica viagem inaugural do Titanic em 1912. 
-      Embora esteja noiva do arrogante herdeiro de uma siderúrgica, a jovem desafia sua família e amigos em busca do verdadeiro amor.`,
-  };
-  
-  const filme2 = {
-    id: 2,
-    nome: "UP: Altas Aventuras",
-    duracao: 96,
-    genero: "animação",
-    sinopse:
-      `Uma animação que conta a história de Carl e Russell que, despropositadamente, partem juntos em busca de um belíssimo
-      lugar chamado Paraíso das Cachoeiras.`,
-  };
-  
-  //Banco de dados
-  const filmes = [filme1, filme2];
+//Objetos iniciais
+const filme1 = new Filme(1, "Titanic", 194, "drama", `Um artista pobre e uma jovem rica se conhecem e se apaixonam na fatídica viagem inaugural do Titanic em 1912. 
+  Embora esteja noiva do arrogante herdeiro de uma siderúrgica, a jovem desafia sua família e amigos em busca do verdadeiro amor.`)
 
-  module.exports = {Filme,filmes};
+
+const filme2 = new Filme(2, "UP: Altas Aventuras", 96, "animação", `Uma animação que conta a história de Carl e Russell que, despropositadamente, partem juntos em busca de um belíssimo
+  lugar chamado Paraíso das Cachoeiras.`)
+
+
+//Banco de dados
+const filmes = [filme1, filme2];
+
+module.exports = { Filme, filmes };
